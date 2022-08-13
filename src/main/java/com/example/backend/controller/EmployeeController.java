370e11283342e11bebd93352a1c3708612c9fe63
laraib-786 <ruk10011997@gmail.com>
2022-08-13 22:45:07 +0530

    Add. API for Add Employee

--- src/main/java/com/example/backend/controller/EmployeeController.java
+++ src/main/java/com/example/backend/controller/EmployeeController.java
@@ -3,9 +3,12 @@ package com.example.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.backend.model.Employee;
@@ -14,15 +17,24 @@ import com.example.backend.repository.EmployeeRepository;
 
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 	@Autowired
 	private EmployeeRepository employeeRepository;
-	@GetMapping(value="/employees")
 	
+	
+	@GetMapping(value="/employees")
 	public List<Employee> getAllEmployees(){
 		List<Employee> employees=employeeRepository.findAll();
 		return employees;
+	}
+	
+	
+	
+	@PostMapping(value="/employees")
+	public Employee allEmployee(@RequestBody Employee employee) {
+		return employeeRepository.save(employee);
 	}
 
 }
