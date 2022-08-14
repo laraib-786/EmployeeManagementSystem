f0671ac6c4275f0389d4faef6f0a59f074c99d3d
laraib-786 <ruk10011997@gmail.com>
2022-08-14 22:11:29 +0530

    Add. API to find employee by id

--- src/main/java/com/example/backend/controller/EmployeeController.java
+++ src/main/java/com/example/backend/controller/EmployeeController.java
@@ -1,16 +1,21 @@
 package com.example.backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend.exception.ResourceNotFoundException;
 import com.example.backend.model.Employee;
 import com.example.backend.repository.EmployeeRepository;
 
@@ -33,8 +38,15 @@ public class EmployeeController {
 	
 	
 	@PostMapping(value="/employees")
-	public Employee allEmployee(@RequestBody Employee employee) {
+	public Employee addEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
+	}
+	
+	@GetMapping(value="/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+		Employee employee=employeeRepository.findById(id).orElseThrow( ()-> new ResourceNotFoundException("Employee does not exist with id: "+id));
+		return ResponseEntity.ok(employee);
+		
 	}
 
 }
